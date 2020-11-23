c03bab34fc74384a72a90725904b611a47fc48b8
Vasyl Sokolyk <vsokolyk@gmail.com>
2020-11-23 19:48:55 +0200

    enable CheckoutValidation class

--- wp1/wp-content/themes/single-way-sync/inc/App.php
+++ wp1/wp-content/themes/single-way-sync/inc/App.php
@@ -3,13 +3,15 @@
 namespace Sync\WpSource;
 
 use Sync\Utils\Gateway\Stripe;
+use Sync\Utils\Wc\CheckoutValidation;
 
 class App
 {
     public function __construct()
     {
         app_autoload_classes([
-			Stripe::class
+			Stripe::class,
+			CheckoutValidation::class
         ]);
     }
 }
